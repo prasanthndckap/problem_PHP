b76ea14bde896aa7e93e6a87eb307d87c252afc2
prasanthndckap <prasanthndckap@gmail.com>
2023-05-27 11:53:49 +0530

    Completed the factorialSeries

--- Factorial.php
+++ Factorial.php
@@ -4,7 +4,7 @@
  $inputNumber = readline("Enter the Numner : ");
 
  $secondValue = $inputNumber;
- while($inputNumber>2){
+ while($inputNumber>1){
      $inputNumber--;
      $secondValue =  $secondValue * $inputNumber;
      echo $secondValue."\n";
